23050b6553737f06bc671794d128cc90f064b3c7
thsthiago <thsthiago04>
2022-01-04 21:53:27 -0300

    adding urls

--- Order-Api/order/api/serializers.py
+++ Order-Api/order/api/serializers.py
@@ -1,17 +1,13 @@
+from django.db.models import fields
 from rest_framework import serializers
-from order import models
+from models import Order, Product 
 
 class OrderSerializer(serializers.ModelSerializer):
   class Meta:
-    model = models.Order
+    model = Order
     fields = '__all__'
-
-class OrderItemSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = models.Order
-    fields = '__all__'
-
+    
 class ProductSerializer(serializers.ModelSerializer):
   class Meta:
-    model = models.Order
+    model = Product
     fields = '__all__'
--- Order-Api/order/views.py
+++ Order-Api/order/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def order(request):
+  if request.method == 'GET':
+    return Response({"message": "Hello world!"})
